0ff00ec21625fa5a5aa17c28b3144d1b67a80bc6
vaidas <vaidas.pilkauskas@gmail.com>
2014-01-19 11:31:55 +0200

    added Specs2 and dummy test

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "track-parser"
+
+version := "1.0"
+
+sbtBinaryVersion := "0.13"
+
+libraryDependencies += "org.specs2" %% "specs2" % "2.3.6" % "test"
---
+++ src/test/scala/lt/overdrive/trackparser/parsing/ParserSpec.scala
@@ -0,0 +1,11 @@
+package lt.overdrive.trackparser.parsing
+
+import org.specs2.mutable.Specification
+
+class ParserSpec extends Specification {
+  "parser" should {
+    "do nothing" in {
+
+    }
+  }
+}
