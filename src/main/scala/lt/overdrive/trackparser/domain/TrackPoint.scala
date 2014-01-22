bee28c5ef2e567517931d6b1ea76740df1a96239
liucijus <vaidas.pilkauskas@gmail.com>
2014-01-22 08:47:48 +0200

    swapped latitude and longitude parameters in correct order

--- src/main/scala/lt/overdrive/trackparser/domain/TrackPoint.scala
+++ src/main/scala/lt/overdrive/trackparser/domain/TrackPoint.scala
@@ -2,4 +2,4 @@ package lt.overdrive.trackparser.domain
 
 import org.joda.time.DateTime
 
-case class TrackPoint(longitude: Double, latitude: Double, altitude: Option[Double], date: Option[DateTime])
+case class TrackPoint(latitude: Double, longitude: Double, altitude: Option[Double], date: Option[DateTime])
