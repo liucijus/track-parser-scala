629582bddca736e03ff282fa04fbd16f6ef072fa
vaidas <vaidas.pilkauskas@gmail.com>
2014-01-28 21:31:50 +0200

    ResourceUtils.getResourceUrl

--- src/main/scala/lt/overdrive/trackparser/utils/ResourceUtils.scala
+++ src/main/scala/lt/overdrive/trackparser/utils/ResourceUtils.scala
@@ -4,6 +4,7 @@ import java.io.File
 import scala.util.Try
 import javax.xml.validation.{SchemaFactory, Schema}
 import javax.xml.XMLConstants
+import java.net.URL
 
 object ResourceUtils {
   def getFile(name: String): Option[File] = try {
@@ -12,8 +13,14 @@ object ResourceUtils {
     case _: Throwable => None
   }
 
+  def getResourceUrl(name: String): Option[URL] = try {
+    Option(Thread.currentThread.getContextClassLoader.getResource(name))
+  } catch {
+    case _: Throwable => None
+  }
+
   def loadSchema(name: String): Try[Schema] = {
     val sf: SchemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-    Try(sf.newSchema(getFile(name).get))
+    Try(sf.newSchema(getResourceUrl(name).get))
   }
 }
